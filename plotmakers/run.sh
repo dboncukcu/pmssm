51d9e7fb77ec893177f9be4c56592f0dc67e62be
dboncukcu <dorukhanboncukcu@gmail.com>
2024-04-19 10:25:24 +0300

    run.sh optimized (thanks to Sam)

--- plotmakers/run.sh
+++ plotmakers/run.sh
@@ -1,13 +1,12 @@
-python3 plotmakers/t1.py
-python3 plotmakers/t2.py
-python3 plotmakers/b1.py
-python3 plotmakers/g.py
-python3 plotmakers/lcsp.py
-python3 plotmakers/abschi10.py
-python3 plotmakers/abschipm.py
-python3 plotmakers/abschi20.py
-python3 plotmakers/abschi1pmchi10.py
-python3 plotmakers/abschi20chi10.py
-python3 plotmakers/delta_masses.py
-
+python3 plotmakers/t1.py > log/t1.txt &
+python3 plotmakers/b1.py > log/b1.txt &
+python3 plotmakers/g.py > log/g.txt &
+python3 plotmakers/lcsp.py > log/lcsp.txt &
+python3 plotmakers/abschi10.py > log/abschi10.txt &
+python3 plotmakers/abschipm.py > log/abschipm.txt &
+python3 plotmakers/abschi20.py > log/abschi20.txt &
+python3 plotmakers/abschi1pmchi10.py > log/abschi1pmchi10.txt &
+python3 plotmakers/abschi20chi10.py > log/abschi20chi10.txt &
+python3 plotmakers/delta_masses.py > log/delta_masses.txt 
+#make sure to wait at thi point that everything finishes, and then run
 python3 plotmakers/collect_plots.py
